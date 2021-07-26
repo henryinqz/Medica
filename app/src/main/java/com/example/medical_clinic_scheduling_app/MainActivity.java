34a904a239ab05512bb763dcf4b3408aa6752b60
Justin <justinx.wang@mail.utoronto.ca>
2021-07-26 18:13:16 -0400

    Justin Test Commit

--- app/src/main/java/com/example/medical_clinic_scheduling_app/MainActivity.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     // Test
+    // Test (Justin)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
