58f80f266a6fb176c84e566f6bb1861040c51a49
Eric Zhou <ericckzhou@gmail.com>
2021-07-27 15:00:36 -0400

    Added DatePicker for patient birthdate & aligned the formatting.

--- app/src/main/java/com/example/medical_clinic_scheduling_app/PatientRegisterActivity.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/PatientRegisterActivity.java
@@ -1,12 +1,21 @@
 package com.example.medical_clinic_scheduling_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class PatientRegisterActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class PatientRegisterActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,28 @@ public class PatientRegisterActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.SelectGender));
         genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(genderSpinnerAdapter);
+
+        //Setting up user birthDateButton Listener
+        Button birthDate = (Button) findViewById(R.id.birthDatePickerButton);
+        birthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "Birth Date Picker");
+            }
+        });
     }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day){
+        //Setting the birthDateLabel to patient's birthday from DatePicker.
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, day);
+        String currentDateStr = DateFormat.getDateInstance().format(c.getTime());
+        TextView textView = (TextView) findViewById(R.id.patientBirthDate);
+        textView.setText(currentDateStr);
+    }
+
 }
