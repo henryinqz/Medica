ff0ec6c5c26820cd8384ecd650e3b90344df28fc
Zhao Ji Wang <mikezerolol20@gmail.com>
2021-07-27 11:24:14 -0400

    Refactored the Styles of Register and Doctor Registration Page. Finalized the Doctor Registration Page.

--- app/src/main/java/com/example/medical_clinic_scheduling_app/DoctorRegisterActivity.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/DoctorRegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.medical_clinic_scheduling_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class DoctorRegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class DoctorRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_register);
+
+        //Setting up the Gender Dropdown List
+        Spinner genderSpinner = (Spinner)findViewById(R.id.Genderspinner);
+        ArrayAdapter<String> genderSpinnerAdapter = new ArrayAdapter<String>(DoctorRegisterActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.SelectGender));
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderSpinnerAdapter);
+
+        //Setting up the Specialist Dropdown List
+        Spinner specialistSpinner = (Spinner)findViewById(R.id.SpecialistSpinner);
+        ArrayAdapter<String> specialistSpinnerAdapter = new ArrayAdapter<String>(DoctorRegisterActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.SelectSpecialist));
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        specialistSpinner.setAdapter(specialistSpinnerAdapter);
+
+
     }
 }
