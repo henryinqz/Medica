c633946f166e31f321a1468b8b0c864d95db59a5
yusiu <deepblues1618@gmail.com>
2021-07-27 00:56:57 -0400

    Added: - UI for Main page - UI for Login page - Code for most buttons - Variables in String.xml

--- app/src/main/java/com/example/medical_clinic_scheduling_app/MainActivity.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.medical_clinic_scheduling_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
     // Test
@@ -13,4 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Login button
+    public void to_login(View view) {
+        Intent intent = new Intent(this, LoginPageActivity.class);
+        startActivity(intent);
+
+    }
+
+//    //Register button (Incomplete)
+//    public void to_register(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        startActivity(intent);
+//    }
+
 }
