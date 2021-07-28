c1623f6816ee3a4a590a87a277565846ae3114b4
Henry <36207470+henryinqz@users.noreply.github.com>
2021-07-27 20:37:11 -0400

    Removed patient/doctor login radio buttons

--- app/src/main/java/com/example/medical_clinic_scheduling_app/LoginPageActivity.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/LoginPageActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 public class LoginPageActivity extends AppCompatActivity {
 
@@ -22,11 +20,11 @@ public class LoginPageActivity extends AppCompatActivity {
 
     public void loginButtonClick(View view) { // Run when login button is pressed
         // Username
-        EditText usernameEditText = (EditText) findViewById(R.id.editTextUsername);
+        EditText usernameEditText = (EditText) findViewById(R.id.editTextLoginUsername);
         String username = usernameEditText.getText().toString();
 
         // Password
-        EditText passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+        EditText passwordEditText = (EditText) findViewById(R.id.editTextLoginPassword);
         String password = passwordEditText.getText().toString();
 
 
@@ -37,15 +35,6 @@ public class LoginPageActivity extends AppCompatActivity {
                 passwordEditText.setError("Empty password");
 //        } else if () { // TODO: Regex to check if username & passwords have correct format (ie. no whitespace, dealing w/ special characters, length of 32 char or smth)
         } else {
-            // Check which user type is selected (patient/doctor)
-            RadioGroup userTypeRadioGroup = (RadioGroup) findViewById(R.id.userTypeRadioGroup);
-            RadioButton userTypeRadioButton = (RadioButton) userTypeRadioGroup.findViewById(userTypeRadioGroup.getCheckedRadioButtonId());
-            if (userTypeRadioButton == (RadioButton) findViewById(R.id.userTypePatientRadioButton)) { // Patient button selected
-                // TODO: Something
-            } else if (userTypeRadioButton == (RadioButton) findViewById(R.id.userTypeDoctorRadioButton)) { // Doctor button selected
-                // TODO: Something
-            }
-
             // TODO: Check that username/password combo is correct w/ Firebase
 
             // TODO: Login (auth token? then go to patient/doctor screen of upcoming appointments)
