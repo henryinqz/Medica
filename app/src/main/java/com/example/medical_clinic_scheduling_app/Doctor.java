22e65a21aaa8ede5f42b117658123241cb968f7e
Henry <36207470+henryinqz@users.noreply.github.com>
2021-08-02 21:24:49 -0400

    Added getters/setters for List types in Doctor/Patient

--- app/src/main/java/com/example/medical_clinic_scheduling_app/Doctor.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/Doctor.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 public class Doctor extends Person implements Observer {
     private List<String> specializations;
-    private List<Integer> upcomingAppointmentIDs;
-    private List<Integer> seenPatientIDs;
+    private List<Integer> upcomingAppointmentIDs, seenPatientIDs;
 
     Doctor(String username, String firstName, String lastName, String gender, HashSet<String> specializations) {
         super(username, firstName, lastName, gender, Constants.PERSON_TYPE_DOCTOR);
@@ -17,32 +16,27 @@ public class Doctor extends Person implements Observer {
         this.seenPatientIDs = new ArrayList<Integer>();
     }
 
+    // Getters/setters:
+    // specializations
     public List<String> getSpecializations() {
         return specializations;
     }
     public void setSpecializations(List<String> specializations) {
         this.specializations = specializations;
     }
-
-    // TODO: Fix crashes when these Set<> getters/setters are uncommented
-//    public Set<Patient> getSeenPatients() {
-//        return seenPatients;
-//    }
-//
-//    public void setSeenPatients(Set<Patient> seenPatients) {
-//        this.seenPatients = seenPatients;
-//    }
-//
-//    public Set<Appointment> getUpcomingAppointments() {
-//        return upcomingAppointments;
-//    }
-//
-//    public void setUpcomingAppointments(Set<Appointment> upcomingAppointments) {
-//        this.upcomingAppointments = upcomingAppointments;
-//    }
-
-    public void addSeenPatient(Patient patient) {
-        seenPatientIDs.add(patient.hashCode());
+    // upcomingAppointmentIDs
+    public List<Integer> getUpcomingAppointmentIDs() {
+        return upcomingAppointmentIDs;
+    }
+    public void addUpcomingAppointment(Appointment upcomingAppt) {
+        this.upcomingAppointmentIDs.add(upcomingAppt.hashCode());
+    }
+    // seenPatientIDs
+    public List<Integer> getSeenPatientIDs() {
+        return seenPatientIDs;
+    }
+    public void addSeenPatient(Patient patient) { // TODO: Maybe private & only accessed by observers after appointment passes?
+        this.seenPatientIDs.add(patient.hashCode());
     }
 
 
@@ -50,7 +44,6 @@ public class Doctor extends Person implements Observer {
     public void updateBooking(Appointment appointment) {
         upcomingAppointmentIDs.add(appointment.hashCode());
     }
-
     @Override
     public void updatePassing(Appointment appointment) {
         upcomingAppointmentIDs.remove(appointment.hashCode());
--- app/src/main/java/com/example/medical_clinic_scheduling_app/Patient.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/Patient.java
@@ -6,9 +6,7 @@ import java.util.Date;
 
 public class Patient extends Person implements Subject {
     private Date dateOfBirth;
-    private List<Integer> prevAppointmentIDs;
-    private List<Integer> upcomingAppointmentIDs;
-    private List<Integer> seenDoctorIDs;
+    private List<Integer> prevAppointmentIDs, upcomingAppointmentIDs, seenDoctorIDs;
     private List<Observer> observers;
 
     Patient(String username, String firstname, String lastname, String gender, Date dateOfBirth) {
@@ -21,16 +19,38 @@ public class Patient extends Person implements Subject {
         this.observers = new ArrayList<Observer>();
     }
 
+    // Getters/setters:
+    // dateOfBirth
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
     public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
-
-    public void addSeenDoctor(Doctor doctor) {
-        seenDoctorIDs.add(doctor.hashCode());
+    // prevAppointmentIDs
+    public List<Integer> getPrevAppointmentIDs() {
+        return prevAppointmentIDs;
     }
+    public void addPrevAppointment(Appointment prevAppt) {
+        this.prevAppointmentIDs.add(prevAppt.hashCode());
+    }
+    // upcomingAppointmentIDs
+    public List<Integer> getUpcomingAppointmentIDs() {
+        return upcomingAppointmentIDs;
+    }
+    public void addUpcomingAppointment(Appointment upcomingAppt) {
+        this.upcomingAppointmentIDs.add(upcomingAppt.hashCode());
+    }
+    // seenDoctorIDs
+    public List<Integer> getSeenDoctorIDs() {
+        return seenDoctorIDs;
+    }
+    public void addSeenDoctor(Doctor doctor) { // TODO: Maybe private & only accessed by observers after appointment passes?
+        this.seenDoctorIDs.add(doctor.hashCode());
+    }
+    // TODO: observers (? Not sure if this should be sent to Firebase)
+
+
 
     public void bookAppointment(Appointment appointment) {
         attach(appointment.doctor);
@@ -38,16 +58,15 @@ public class Patient extends Person implements Subject {
         notifyBooking(appointment);
     }
 
+    // Observers
     @Override
     public void attach(Observer o) {
         observers.add(o);
     }
-
     @Override
     public void dettach(Observer o) {
         observers.remove(o);
     }
-
     @Override
     public void notifyBooking(Appointment appointment) {
         for (Observer o : observers) {
