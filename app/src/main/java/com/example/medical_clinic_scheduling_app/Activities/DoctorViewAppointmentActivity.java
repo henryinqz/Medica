abd6524368d8cdcea036603947c522f6ab848283
Henry <36207470+henryinqz@users.noreply.github.com>
2021-08-07 18:21:38 -0400

    Fixed crashing on doctor upcoming appts

--- app/src/main/java/com/example/medical_clinic_scheduling_app/Activities/DoctorViewAppointmentActivity.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/Activities/DoctorViewAppointmentActivity.java
@@ -35,8 +35,8 @@ public class DoctorViewAppointmentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_view_appointment);
 
         //Setting up ListView of Appointments
-        ArrayList<String> appointments = new ArrayList<String>();
-        ArrayList<String> appointmentIDs = new ArrayList<String>();
+        ArrayList<String> appointments = new ArrayList<>();
+//        ArrayList<String> appointmentIDs = new ArrayList<String>();
         ListView appointmentsView = (ListView) findViewById(R.id.AppointmentListView);
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -63,7 +63,7 @@ public class DoctorViewAppointmentActivity extends AppCompatActivity {
                                     if (patientID.equals(childID) && !date.before(today)) {
                                         Person patient = child.getValue(Person.class);
                                         appointments.add("Patient " + patient.toString() + "\n" + date.toString());
-                                        appointmentIDs.add(child.child(Constants.FIREBASE_PATH_APPOINTMENT_ID).getValue(String.class));
+//                                        appointmentIDs.add(child.child(Constants.FIREBASE_PATH_APPOINTMENT_ID).getValue(String.class));
                                     }
                                 }
                                 ArrayAdapter appointmentAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, appointments);
@@ -90,7 +90,8 @@ public class DoctorViewAppointmentActivity extends AppCompatActivity {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                     Intent intent = new Intent(getApplicationContext(), DoctorViewAppointmentDetailsActivity.class);
-                    intent.putExtra("Appointment", appointmentIDs.get(i));
+                    intent.putExtra("Appointment", appointments.get(i));
+//                    intent.putExtra("Appointment", appointmentIDs.get(i));
                     startActivity(intent);
                 }
             });
