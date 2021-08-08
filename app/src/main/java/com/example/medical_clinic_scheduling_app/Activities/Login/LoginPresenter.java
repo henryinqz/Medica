bc420bcc25e721033b465d7a5a36cc32d4b3eba3
Henry <36207470+henryinqz@users.noreply.github.com>
2021-08-07 21:16:23 -0400

    Moved login() to LoginModel using LoginListener

--- app/src/main/java/com/example/medical_clinic_scheduling_app/Activities/Login/LoginPresenter.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/Activities/Login/LoginPresenter.java
@@ -1,96 +1,93 @@
 package com.example.medical_clinic_scheduling_app.Activities.Login;
 
-import android.content.Intent;
-import android.util.Log;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-
-import com.example.medical_clinic_scheduling_app.Activities.UserHomeActivity;
-import com.example.medical_clinic_scheduling_app.Constants;
-import com.example.medical_clinic_scheduling_app.Objects.Person;
 import com.example.medical_clinic_scheduling_app.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-public class LoginPresenter {
+public class LoginPresenter implements LoginListener {
     private LoginModel model;
     private LoginViewActivity view;
 
-    public LoginPresenter(LoginModel model, LoginViewActivity view) {
-        this.model = model;
+    public LoginPresenter(LoginViewActivity view) {
         this.view = view;
+        this.model = new LoginModel(this);
     }
 
     public void checkCredentials() {
         String username = this.view.getUsername();
         String password = this.view.getPassword();
         if (username.isEmpty())
-            this.view.displayError((TextView) this.view.findViewById(R.id.editTextLoginUsername), "Username cannot be empty");
+            this.view.displayError("Username cannot be empty", (TextView) this.view.findViewById(R.id.editTextLoginUsername));
         else if (password.isEmpty())
-            this.view.displayError((TextView) this.view.findViewById(R.id.editTextLoginPassword), "Password cannot be empty");
+            this.view.displayError("Password cannot be empty", (TextView) this.view.findViewById(R.id.editTextLoginPassword));
         else
-            login();
+            login(username, password);
 
     }
 
-    public void login() {
-        String username = this.view.getUsername();
-        String password = this.view.getPassword();
-
-        String emailUsername = username + Constants.USERNAME_EMAIL_DOMAIN;
-
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.signInWithEmailAndPassword(emailUsername, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) { // Logged in
-                    view.displayMessage("Logged in");
-
-                    String userID = auth.getCurrentUser().getUid();
-                    FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_PATH_USERS)
-                            .child(userID)
-                            .addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                    Person user = snapshot.getValue(Person.class);
-                                    if (user != null) {
-                                        Intent intent = new Intent(view.getApplicationContext(), UserHomeActivity.class);
-                                        intent.putExtra("userid", userID);
-                                        view.startActivity(intent);
-                                    } else { // user == null
-                                        view.displayMessage("Error: User is empty");
-                                    }
-                                }
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) { // Failed to get user
-                                    view.displayMessage("Error: Failed to get user");
-                                }
-                            });
-                } else { // Failed to login
-                    try {
-                        switch(task.getException().getMessage()) {
-                            case "There is no user record corresponding to this identifier. The user may have been deleted.":
-                                view.displayError((TextView) view.findViewById(R.id.editTextLoginUsername), "Username does not exist");
-                                break;
-                            case "The password is invalid or the user does not have a password.":
-                                view.displayError((TextView) view.findViewById(R.id.editTextLoginPassword), "Invalid password");
-                                break;
-                            case "A network error (such as timeout, interrupted connection or unreachable host) has occurred.":
-                                view.displayMessage("Error: Could not connect to the server");
-                                break;
-                        }
-                    } catch (NullPointerException e) {
-                        view.displayMessage("Error: Failed to login");
-                    }
-                }
-            }
-        });
+    public void login(String username, String password) {
+        this.model.login(username, password);
     }
+
+    public void loginSuccess() {
+        this.view.displayMessage("Logged in");
+        this.view.startUserHomeActivity();
+    }
+    public void loginFailed(String errorMessage) {
+        view.displayMessage("Error: " + errorMessage);
+    }
+
+//    public void login() {
+//        String username = this.view.getUsername();
+//        String password = this.view.getPassword();
+//
+//        String emailUsername = username + Constants.USERNAME_EMAIL_DOMAIN;
+//
+//        FirebaseAuth auth = FirebaseAuth.getInstance();
+//        auth.signInWithEmailAndPassword(emailUsername, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) { // Logged in
+//                    view.displayMessage("Logged in");
+//
+//                    String userID = auth.getCurrentUser().getUid();
+//                    FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_PATH_USERS)
+//                            .child(userID)
+//                            .addListenerForSingleValueEvent(new ValueEventListener() {
+//                                @Override
+//                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                                    Person user = snapshot.getValue(Person.class);
+//                                    if (user != null) {
+//                                        Intent intent = new Intent(view.getApplicationContext(), UserHomeActivity.class);
+//                                        intent.putExtra("userid", userID);
+//                                        view.startActivity(intent);
+//                                    } else { // user == null
+//                                        view.displayMessage("Error: User is empty");
+//                                    }
+//                                }
+//                                @Override
+//                                public void onCancelled(@NonNull DatabaseError error) { // Failed to get user
+//                                    view.displayMessage("Error: Failed to get user");
+//                                }
+//                            });
+//                } else { // Failed to login
+//                    try {
+//                        switch(task.getException().getMessage()) {
+//                            case "There is no user record corresponding to this identifier. The user may have been deleted.":
+//                                view.displayError((TextView) view.findViewById(R.id.editTextLoginUsername), "Username does not exist");
+//                                break;
+//                            case "The password is invalid or the user does not have a password.":
+//                                view.displayError((TextView) view.findViewById(R.id.editTextLoginPassword), "Invalid password");
+//                                break;
+//                            case "A network error (such as timeout, interrupted connection or unreachable host) has occurred.":
+//                                view.displayMessage("Error: Could not connect to the server");
+//                                break;
+//                        }
+//                    } catch (NullPointerException e) {
+//                        view.displayMessage("Error: Failed to login");
+//                    }
+//                }
+//            }
+//        });
+//    }
 }
--- app/src/main/java/com/example/medical_clinic_scheduling_app/Activities/Login/LoginViewActivity.java
+++ app/src/main/java/com/example/medical_clinic_scheduling_app/Activities/Login/LoginViewActivity.java
@@ -2,13 +2,16 @@ package com.example.medical_clinic_scheduling_app.Activities.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.medical_clinic_scheduling_app.Activities.UserHomeActivity;
 import com.example.medical_clinic_scheduling_app.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginViewActivity extends AppCompatActivity {
     private LoginPresenter presenter;
@@ -18,7 +21,7 @@ public class LoginViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-        this.presenter = new LoginPresenter(new LoginModel(), this);
+        this.presenter = new LoginPresenter(this);
     }
 
     public void onClick(View view) {
@@ -32,7 +35,7 @@ public class LoginViewActivity extends AppCompatActivity {
     public void displayMessage(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
-    public void displayError(TextView textView, String errorMessage) {
+    public void displayError(String errorMessage, TextView textView) {
         textView.setError(errorMessage);
     }
 
@@ -45,5 +48,14 @@ public class LoginViewActivity extends AppCompatActivity {
         EditText passwordEditText = (EditText) findViewById(R.id.editTextLoginPassword);
         String password = passwordEditText.getText().toString().trim();
         return password;
+    }
+
+    public void startUserHomeActivity() {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) { // Go to user home
+            Intent intent = new Intent(getApplicationContext(), UserHomeActivity.class);
+            startActivity(intent);
+        } else {
+            displayMessage("Error: User is not logged in");
+        }
     }
 }
