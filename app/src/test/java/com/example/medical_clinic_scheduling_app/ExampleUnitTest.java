51f32dd0a6fd291aed70a2d4c54845a5f8972383
Henry <36207470+henryinqz@users.noreply.github.com>
2021-08-08 23:11:26 -0400

    Added Mockito dependency

--- app/src/test/java/com/example/medical_clinic_scheduling_app/ExampleUnitTest.java
+++ app/src/test/java/com/example/medical_clinic_scheduling_app/ExampleUnitTest.java
@@ -1,17 +1,32 @@
 package com.example.medical_clinic_scheduling_app;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+
+import com.example.medical_clinic_scheduling_app.Activities.Login.LoginModel;
+import com.example.medical_clinic_scheduling_app.Activities.Login.LoginViewActivity;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+    @Mock
+    LoginModel model;
+
+    @Mock
+    LoginViewActivity view;
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testPresenter() {
+
     }
 }
